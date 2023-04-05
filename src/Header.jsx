e7af2df83794a88eb134043a77106abbc76ff4e6
Mace Akkari <mace.akkari@relyfe.com>
2023-04-05 12:15:01 +0100

    add header styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,9 +4,6 @@ import logo from "./images/Underground.svg";
 function Header() {
   return (
     <div className="header">
-      <div className="header_logo">
-        <img src={logo} alt="Logo" />
-      </div>
       <div className="header_title">Tube Timetable</div>
       <div className="header_logo">
         <img src={logo} alt="Logo" />
